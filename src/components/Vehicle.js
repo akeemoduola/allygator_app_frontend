f5aaddefcba0d9d499fd665541540037bca8f2d1
Akeem Oduola <akeemoduola@Akeems-MacBook-Pro.local>
2019-08-28 16:38:41 +0100

    Create Vehicle component and add Map and Markers logic

---
+++ src/components/Vehicle.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import VehiclesList from "./VehicleList";
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class Vehicle extends Component {
+  render() {
+    const google = this.props.google
+
+    return (
+      <VehiclesList>
+        {vehicles => {
+          return (
+            <div>
+              <Map
+                google={google}
+                zoom={8}
+                style={mapStyles}
+                initialCenter={{
+                 lat: 52.53,
+                 lng: 13.403
+                }}
+              >
+                {markedVehicles(vehicles).map((value, index) => {
+                  return value
+                })}
+              </Map>
+          </div>
+        );}}
+      </VehiclesList>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCFHiUzdr2CO3YvXP5UEdnNi1ewHXXxtQo'
+})(Vehicle);
+
+function markedVehicles(vehicles) {
+  var marked = []
+  for (const key in vehicles) {
+    var vehicle = vehicles[key]
+    marked.push(renderVehichle(vehicle))
+  }
+  return marked;
+}
+
+function renderVehichle(vehicle) {
+  if (!!vehicle.current_latitude) {
+    var currentLatitude = Number(vehicle.current_latitude)
+    var currentLongitude = Number(vehicle.current_longitude)
+    return <Marker position={{ lat: currentLatitude, lng: currentLongitude}} />
+  }
+}
